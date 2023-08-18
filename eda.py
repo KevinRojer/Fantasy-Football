2181706faacf9ef73f6c9814c70fb56cf8a3a2a1
Kevin Rojer <kevin.rojer@live.com>
2023-08-18 17:08:26 +0200

    Create eda.py

---
+++ eda.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 18 16:00:05 2023
+
+@author: kevinrojer
+"""
+
+
+import pandas as pd
+import numpy as np
+
+
+def main():
+    print("Hello World!")
+    
+    
+if __name__ == "__main__":
+    main()
